a00c3bf0bd5a234456467716f7166ea453e9380d
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-11-04 12:51:17 +0600

    Update ProductsModel.js

--- backend/src/models/ProductsModel.js
+++ backend/src/models/ProductsModel.js
@@ -5,6 +5,9 @@ const DataSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    image: {
+      type: String,
+    },
     description: {
       type: String,
       required: true,
